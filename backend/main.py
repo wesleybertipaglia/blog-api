743b11dbe58298207169f9a76500fcade0a7e6fa
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-11 10:22:10 -0300

    feat: add new entities routes

--- backend/main.py
+++ backend/main.py
@@ -29,3 +29,8 @@ def home():
 
 # routes
 app.include_router(user_router, prefix='/users', tags=['Users'])
+app.include_router(post_router, prefix='/posts', tags=['Posts'])
+app.include_router(comment_router, prefix='/comments', tags=['Comments'])
+app.include_router(like_router, prefix='/likes', tags=['Likes'])
+app.include_router(follow_router, prefix='/follows', tags=['Follows'])
+
