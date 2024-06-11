c81e2cacf3b6cdc62a8276a37498d88413df35b4
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-10 21:23:58 -0300

    feat: update user model

--- backend/src/models/user.py
+++ backend/src/models/user.py
@@ -1,4 +1,5 @@
 """User model module."""
+
 from datetime import datetime
 from uuid import uuid4
 from sqlalchemy import String, DateTime
@@ -9,14 +10,14 @@ class UserModel (Base):
     """User model class"""
     __tablename__ = 'users'
     
-    id: Mapped[str] = mapped_column(String, primary_key=True, index=True, default=lambda: str(uuid4()))    
-    username: Mapped[str] = mapped_column(String(14))    
-    email: Mapped[str] = mapped_column(String(100))        
-    password: Mapped[str] = mapped_column(String(100))
-    name: Mapped[str] = mapped_column(String(100))
-    bio: Mapped[str] = mapped_column(String(255))
-    picture: Mapped[str] = mapped_column(String(255))
-    location: Mapped[str] = mapped_column(String(255))
-    link: Mapped[str] = mapped_column(String(255))    
+    id: Mapped[str] = mapped_column(String, primary_key=True, index=True, default=lambda: str(uuid4()))
+    username: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(100), nullable=False)
+    name: Mapped[str] = mapped_column(String(100), nullable=True)
+    bio: Mapped[str] = mapped_column(String(255), nullable=True)
+    picture: Mapped[str] = mapped_column(String(255), nullable=True)
+    location: Mapped[str] = mapped_column(String(255), nullable=True)
+    link: Mapped[str] = mapped_column(String(255), nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
