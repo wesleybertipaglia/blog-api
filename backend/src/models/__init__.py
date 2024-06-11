d5908f0c8cb8461c2bee59566e2b14ee41a7ce1e
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-11 10:23:53 -0300

    feat: document and implement packages

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,1 +1,7 @@
-from .user import *
+"""Models package."""
+
+from .user import UserModel
+from .post import PostModel
+from .like import LikeModel
+from .comment import CommentModel
+from .follow import FollowModel
--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -1,1 +1,7 @@
+"""Routes package."""
+
 from .user import router as user_router
+from .post import router as post_router
+from .like import router as like_router
+from .comment import router as comment_router
+from .follow import router as follow_router
